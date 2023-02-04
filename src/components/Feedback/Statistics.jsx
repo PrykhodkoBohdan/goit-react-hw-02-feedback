04fcbd6dfa6d832fc404c8a3a1f0506a454989fc
pickfix <Prikhodko.B.T@gmail.com>
2023-02-04 18:56:46 +0200

    refactor to hooks

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Feedback.module.css';
 
+
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) =>{ 
   return (
   
